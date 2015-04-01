d11530e5ea5e2e1b558ec0712861bbba8a72144c
schutzer <erichschutz@gmail.com>
2015-03-31 22:05:32 -0400

    first checkin

---
+++ js/resumeBuilder.js
@@ -0,0 +1,18 @@
+var bio = {
+    name : "Erich Schutz",
+    email : "erichschutz@gmail.com"
+};
+
+var education = {
+    school : "university of pittsburgh",
+    degree : "Information Science",
+    location : "Pittsburgh, Pa"
+};
+
+var work = {
+    occupation : "software engineer"
+};
+
+var projects = {
+
+};
