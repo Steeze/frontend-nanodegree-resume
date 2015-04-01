afa268562adc84984a60f804dfea5b5b37cade47
schutzer <erichschutz@gmail.com>
2015-03-31 22:37:27 -0400

    update for JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,18 +1,71 @@
 var bio = {
     name : "Erich Schutz",
-    email : "erichschutz@gmail.com"
+    role : "Software Engineer",
+    contacts : {
+        mobile: "206-551-3646",
+        email: "erichschutz@gmail.com",
+        github: "https://github.com/Steeze",
+        twitter: "@ErichSchutz",
+        location: "Pittsburgh, Pa"
+        },
+        welcomeMessage: "Yo Yo Yo",
+        skills: ["JavaScript", "C#", "Codes"],
+        biopic: "images\fry.jpg",
+        display: function(){
+
+       }
 };
 
 var education = {
-    school : "university of pittsburgh",
-    degree : "Information Science",
-    location : "Pittsburgh, Pa"
+    schools: [{
+        name: "University Of Pittsburgh",
+        location: "Pittsburgh, Pa",
+        degree: "Computer Science",
+        majors: ["Psychology", "Information Science"],
+        dates: "",
+        url: "http://www.pitt.edu/"
+      }],
+    onlineCourses: [{
+            title: "Front End Nano-degree",
+            school: "Udacity",
+            date: "",
+            url: "www.udacity.com"
+        }],
+        display: function(){
+
+        }
 };
 
 var work = {
-    occupation : "software engineer"
+    jobs: [
+        {
+        employer: "Concurrent Technology Corporation",
+        title: "Software Engineer",
+        location: "Seattle, Wa",
+        dates: "01/01/01,12/12/12",
+        description: "DOD contractor"
+        },
+        {
+        employer: "Aesynt, Inc",
+        title: "Software Engineer",
+        location: "Pittsburgh, Pa",
+        dates: "12/12/12,04/12/15",
+        description: "Code Slinger"
+        }
+      ],
+    display: function(){
+
+    }
 };
 
 var projects = {
+    projects: [{
+        title: "mploir",
+        dates: "02/01/2015,03/01/2015",
+        description: "building the future of unemployment",
+        images: "array with string urls"
+        }],
+    display: function(){
 
+    }
 };
